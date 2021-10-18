14e4ea75c0d79e1e47a4eda8e7d629eaf300e0ab
zhaozhen <zhaozhen@uniontech.com>
2021-10-18 11:25:51 +0800

    add build source list

---
+++ apt-filesearch/README.md
@@ -0,0 +1,1 @@
+g++ -o conf apt-private/*.cc conf.cpp -I./ -lapt-pkg
---
+++ apt-filesearch/conf.cpp
@@ -0,0 +1,59 @@
+/*
+   supported at least c++ 11
+*/
+#include <apt-pkg/acquire-item.h>
+#include <apt-pkg/acquire.h>
+#include <apt-pkg/cachefile.h>
+#include <apt-pkg/cmndline.h>
+#include <apt-pkg/configuration.h>
+#include <apt-pkg/error.h>
+#include <apt-pkg/fileutl.h>
+#include <apt-pkg/metaindex.h>
+#include <apt-pkg/sourcelist.h>
+#include <apt-pkg/update.h>
+
+#include "apt-private/acqprogress.h"
+#include "apt-private/private-download.h"
+#include "apt-private/private-output.h"
+
+#include <ostream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+int main() {
+   pkgInitConfig(*_config);
+   pkgInitSystem(*_config, _system);
+   pkgCacheFile Cache;
+
+   // Get the source list
+   if (Cache.BuildSourceList() == false) {
+      _error->Error("can not build source list");
+   }
+   pkgSourceList *List = Cache.GetSourceList();
+   _config->CndSet("Acquire::ForceHash", "md5sum");
+
+   // Populate it with the source selection and get all Indexes
+   // (GetAll=true)
+   aptAcquireWithTextStatus Fetcher;
+   if (List->GetIndexes(&Fetcher,true) == false) {
+      cout << "can not get source indexes" << endl;
+   }
+   string compExt = APT::Configuration::getCompressionTypes()[0];
+   pkgAcquire::UriIterator I = Fetcher.UriBegin();
+   for (; I != Fetcher.UriEnd(); ++I)
+   {
+      string FileName = flNotDir(I->Owner->DestFile);
+      if(compExt.empty() == false && APT::String::Endswith(FileName, compExt)) {
+         FileName = FileName.substr(0, FileName.size() - compExt.size() - 1);
+         regex r("Contents");
+         if (regex_search(I->URI, r) == true) {
+            cout << "\"" << I->URI << "\" \"" << FileName << "\"" << endl;
+         }
+      }
+   }
+
+   return 0;
+}
+
