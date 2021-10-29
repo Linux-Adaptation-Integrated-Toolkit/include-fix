602df2486be7dfd310bfd8d3acec592528003440
Maicss <maicss@126.com>
2021-10-29 16:24:44 +0800

    优化 数据列表解析模块

--- apt-filesearch/sfic/main.cpp
+++ apt-filesearch/sfic/main.cpp
@@ -8,8 +8,7 @@ struct entry
     string pkgname;
 };
 // 作用：在文件中逐行查找在数组的第一个元素中包含所需内容的项，并返回结果
-
-vector<entry> ParseContents(string filename, char** targets)
+vector<entry> ParseContents(const string &filename, vector<string> targets)
 {
     fstream file;
     vector<entry> res;
@@ -19,52 +18,39 @@ vector<entry> ParseContents(string filename, char** targets)
         cout << "文件打开失败:"<<filename<<"\n";
         return res;
     }
-    int tarnum =0;
-    char *p=targets[0];
-    while (p)
-    {
-        tarnum++;
-        p=targets[tarnum];
-    }
-    
-    if (!tarnum)
-    {
-        return res;
-    }
     string line;
-    int linenum = 0;
     while (getline(file, line))
     {
-        linenum++;
-        int index;
-        index = line.find("\t");
-        if (index == line.npos)
+        uint index;
+        index = line.find('\t');
+        if (int(index) == string::npos)
         {
-            index = line.find(" ");
+            index = line.find(' ');
         }
         string opt_left = line.substr(0, index);
-        int lastindex = opt_left.find_last_of("/");
-        if (lastindex == opt_left.npos)
+        uint lastindex = opt_left.find_last_of('/');
+        if (int(lastindex) == string::npos)
             lastindex = -1;
-        string filename = opt_left.substr(lastindex + 1, opt_left.length() - lastindex);
-        for (int i = 0; i < tarnum; i++)
+        string file_name = opt_left.substr(lastindex + 1, opt_left.length() - lastindex);
+        vector<string>::iterator it;
+        for (it = targets.begin();it!=targets.end(); it++)
         {
-            if (!filename.compare(targets[i]))
+            if (file_name==*it)
             {
-                index = line.find_last_of("\t");
-                int index2 = line.find_last_of(" ");
-                if (index == line.npos)
+                index = line.find_last_of('\t');
+                uint index2 = line.find_last_of(' ');
+                if (int(index) == string::npos)
                 {
                     index = line.length();
                 }
-                if(index2 == line.npos)
+                if(int(index2) == string::npos)
                 {
                     index2 = line.length();
                 }
                 index = min(index,index2)+1;
                 string opt_right = line.substr(index, line.length() - index);
-                lastindex = opt_right.find_last_of("/");
-                if (lastindex == opt_right.npos)
+                lastindex = opt_right.find_last_of(' ');
+                if (int(lastindex) == string::npos)
                     lastindex = -1;
                 string pkgname = opt_right.substr(lastindex + 1, opt_right.length() - lastindex);
                 entry a;
@@ -77,24 +63,18 @@ vector<entry> ParseContents(string filename, char** targets)
     file.close();
     return res;
 }
-int main(int argc, char **argv)
+int main(int argc, char* argv[])
 {
 
-    char* targets[argc - 1];
-    for (int i = 2; i < argc; i++)
-    {
-        targets[i - 2] = argv[i];
+    vector<string> targets;
+    for(int i=2;i<argc;i++){
+        targets.emplace_back(argv[i]);
     }
-    targets[argc-1]=nullptr;
-    if(argc<3){
-        cout<<"参数过少";
-    }
-    
     vector<entry> a = ParseContents(argv[1], targets);
     for (entry i : a)
     {
         cout << i.pkgname << ": " << i.path << endl;
     }
-    
+
     return 0;
 }
