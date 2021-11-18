682604f827a5b0017ec7c2821d85e422de71f234
zhaozhen <zhaozhen@uniontech.com>
2021-11-18 12:32:49 +0800

    remove libdpkg and add xmake building system

--- wali-dpkgs.h
+++
@@ -1,9 +0,0 @@
-/* copy from config.h of dpkg source code after
- * ./configure
-*/
-#ifndef LIBDPKG_VOLATILE_API
-  #define LIBDPKG_VOLATILE_API 1
-#endif
-
-int searchoutput(struct fsys_namenode *namenode);
-int searchfiles(char** argv);
